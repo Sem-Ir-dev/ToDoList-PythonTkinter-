78eb6585bee9436194514bdc90d6ceea77afac20
Sem_Ir <semi.r04.sr@gmail.com>
2021-08-25 21:58:20 +0500

    New design

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from tkinter import *
 
 class Window:
     def __init__(self, width, height, x, y, title, resizable=(False, False), icon=None):
+        self.white = '#fff'
+        self.bt_bg = '#8D4AEA'
+        self.input_bg = '#F3F1F4'
+
         self.root = Tk()
         self.root.geometry(f'{width}x{height}+{x}+{y}')
         self.root.title(title)
         self.root.resizable(resizable[0], resizable[1])
         self.root.iconbitmap(icon)
+        self.root['bg'] = self.white
 
     def draw_widget(self):
         def bt_add():
@@ -22,27 +27,29 @@ class Window:
             l_list.delete(selected)
 
         # ======== Label ==========
-        l_title = Label(self.root, text='ToDo List', font='Arial 18')
+        l_title = Label(self.root, text='ToDo List', font='ComicSansMs 18', bg=self.white)
         l_title.place(x=250, y=0)
 
-        list_title = Label(self.root, text='Tasks:', font='Arial 14')
+        list_title = Label(self.root, text='Tasks:', font='ComicSansMs 14', bg=self.white)
         list_title.place(x=10, y=30)
 
-        list_title = Label(self.root, text='New Task:', font='Arial 14')
+        list_title = Label(self.root, text='New Task:', font='ComicSansMs 14', bg=self.white)
         list_title.place(x=10, y=340)
 
         # ======== Input ==========
-        l_list = Listbox(self.root, width=55, height=14, font='Arial 12')
+        l_list = Listbox(self.root, width=55, height=14, font='ComicSansMs 12', bg=self.input_bg)
         l_list.place(x=10, y=60)
 
-        e_input = Entry(self.root, width=55, font='Arial 12')
+        e_input = Entry(self.root, width=55, font='ComicSansMs 12', bd=1, bg=self.input_bg)
         e_input.place(x=10, y=370)
 
         # ======== Button ==========
-        bt_delete = Button(self.root, text='Delete', command=bt_delete)
+        bt_delete = Button(self.root, text='Delete', command=bt_delete, bg=self.bt_bg, fg=self.white, bd=0,
+                           font='ComicSansMS 12')
         bt_delete.place(x=525, y=60)
 
-        bt_add = Button(self.root, text='Add', command=bt_add)
+        bt_add = Button(self.root, text='Add', command=bt_add, bg=self.bt_bg, fg=self.white, bd=0,
+                           font='ComicSansMS 12')
         bt_add.place(x=525, y=366)
 
     def run(self):
